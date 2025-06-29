43ba4538b382316113a7801efe77924e94d4a4b5
mickymarsh <162584880+mickymarsh@users.noreply.github.com>
2025-06-29 13:51:55 +0530

    Checking seats availability

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const app = express()
 import loginRoutes from "./routes/login.js"
 import registerRoutes from "./routes/register.js"
 import searchRoutes from "./routes/search.js"
+import seatRoutes from "./routes/seat.js"
+
 
 app.use(express.json())
 app.use(cors(
@@ -19,6 +21,8 @@ app.use(cookieParser())
 app.use("/api/Login", loginRoutes)
 app.use("/api", registerRoutes)
 app.use("/api", searchRoutes)
+app.use("/api", seatRoutes)
+
 
 app.get("/", (req, res) => {
     res.send("Welcome to the API!");
--- vite-project/src/context/Fliightcontext.jsx
+++ vite-project/src/context/Fliightcontext.jsx
@@ -4,9 +4,10 @@ const FlightContext = createContext();
 
 export const FlightProvider = ({ children }) => {
   const [flightId, setFlightId] = useState(null);
+  const [scheduleId, setScheduleId] = useState(null);
 
   return (
-    <FlightContext.Provider value={{ flightId, setFlightId }}>
+    <FlightContext.Provider value={{ flightId, setFlightId, scheduleId, setScheduleId }}>
       {children}
     </FlightContext.Provider>
   );
